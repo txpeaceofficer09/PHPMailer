8455f5edb5d1798fff28552ee9439832f089ea71
txpeaceofficer09 <james.mccaughey@gmail.com>
2022-04-22 13:01:17 -0500

    Update PHPMailer.class.php

--- PHPMailer.class.php
+++ PHPMailer.class.php
@@ -8,10 +8,11 @@ class PHPMailer {
 	var $errno;
 	var $errstr;
 	var $timeout;
+	var $start;
 
 	function __construct($host="yourdomain.com", $port=25, $from="<no-reply@yourdomain.com>") { // Initialize class
-		if ( substr($from, 0, 1) != '<' ) $from = '<'.$from;
-		if ( substr($from, strlen($from)-1, 1) != '>' ) $from = $from.'>';
+		if ( substr($from, 0, 1) != '<' ) $from = '<'.$from; // Add < to the beginning of the e-mail address if it is missing
+		if ( substr($from, strlen($from)-1, 1) != '>' ) $from = $from.'>'; // Add > to the end of the e-mail address if it is missing
 
 		$this->host = $host;
 		$this->port = $port;
@@ -20,6 +21,7 @@ class PHPMailer {
 		$this->errno = null;
 		$this->errstr = null;
 		$this->timeout = 0.02;
+		$this->start = null;
 	}
 
 	private function open() {
@@ -34,11 +36,23 @@ class PHPMailer {
 		fclose($this->stream);
 	}
 
+	private function feof() {
+		$this->start = microtime(true);
+
+		return feof($this->stream);
+	}
+
 	private function read() { // Function to read response from last command send to server.
 		$retVal = "";
-
-		while (substr($retVal, strlen($retVal)-1) != "\n") { // While last byte of $retVal is not a line feed, do...
-			$retVal .= fgets($this->stream, 4); // Read 4 bytes from stream and append to $retVal.
+		$timeout = ini_get('default_socket_timeout');
+		/*
+		while (!$this->feof() && (microtime(true) - $this->start) < $timeout) {
+			$retVal .= fgets($this->stream, 4);
+		}
+		$this->start = null;
+		*/
+		while (substr($retVal, strlen($retVal)-1) != "\n") { // While end of retVal is not a line feed, do...
+			$retVal .= fgets($this->stream, 4); // Read 4 bytes from the stream.
 		}
 
 		return $retVal; // Return our results.
@@ -51,8 +65,8 @@ class PHPMailer {
 	public function send($to, $subject, $message) { // Function to actually send e-mail.
 		$retVal = false; // Initialize the retVal as false. We will set it to true if we successfully send the e-mail.
 
-		if ( substr($to, 0, 1) != '<' ) $to = '<'.$to;
-		if ( substr($to, strlen($to)-1, 1) != '>' ) $to = $to.'>';
+		if ( substr($to, 0, 1) != '<' ) $to = '<'.$to; // Add < to the beginning of the e-mail address if it is missing
+		if ( substr($to, strlen($to)-1, 1) != '>' ) $to = $to.'>'; // Add > to the end of the e-mail address if it is missing
 
 		$cmds = [
 			"HELO ".$this->host."\r\n",
@@ -68,7 +82,11 @@ class PHPMailer {
 			foreach ($cmds AS $cmd) { // Iterate through our list of commands to send to the mail server in order.
 				$this->write($cmd); // Send a command to the server.
 				$result = $this->read(); // Get the server's response to our command.
-				if ( trim($cmd) == 'QUIT' && stripos($result, "250 2.6.0") !== false ) $retVal = true; // If the server responds with a '250 2.6.0' message after we send the QUIT command, we know the message was queued for sending successfully.
+				echo ">> $cmd\n";
+				echo "<< $result\n";
+				if ( stripos($result, "250 2.6.0") !== false ) $retVal = true; // If the server responds with a '250 2.6.0' message (no matter what the wording of the response) that is a mail queued for delivery message and that means success so set our return value to true.
+				if ( stripos($result, "Queued mail for delivery") !== false) $retVal = true; // If the server responds with a message containing 'Queued mail for delivery' that is a success, so set the return value to true.
+				if ( stripos($result, "queued as") !== false ) $retVal = true; // If the server responds with a message containing 'queued as' that is a success, so set the return value to true.
 			}
 			$this->close(); // Close the connection to the mail server.
 		}
